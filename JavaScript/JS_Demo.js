0c981547c26a02f5a3db1b02c49e5eba3c230526
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2022-09-21 22:10:35 +0530

    JS_Demo

---
+++ JavaScript/JS_Demo.js
@@ -0,0 +1,36 @@
+// function first()
+// {
+//     console.log("First method");
+// }
+// function Second()
+// {
+//     console.log("Second method");
+// }
+// function third()
+// {
+//     console.log("Third method");
+// }
+// first();
+// Second();
+// third();
+
+
+
+function first()
+{
+    console.log("First method");
+Second();
+
+}
+function Second()
+{
+    console.log("Second method");
+    third();
+
+}
+function third()
+{
+    console.log("Third method");
+}
+first();
+
