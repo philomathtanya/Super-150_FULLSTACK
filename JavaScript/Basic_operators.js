d80f35321a1d4dd921ac752705e3e69cea22956e
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2022-10-03 22:02:28 +0530

    Basic_operators

---
+++ JavaScript/Basic_operators.js
@@ -0,0 +1,25 @@
+
+let ans = 1 && 1;//print 1 because both values are true
+alert(ans);
+
+
+let Ans = 1 && 0;//print 0 because both values are  not true
+alert(Ans);
+
+let s = 1 || 0;//print 1 because one values is true from  both
+alert(s);
+
+let se = 0 || 0;//print 0 because no values is true from  both
+alert(se);
+
+let x = 0 && 0 && 1 && 1; //print 0
+document.write(x);
+
+document.write('true' > 'trie');// return true
+
+let p = 1;
+document.write(++p);// pre increment operator print 2
+
+
+let q = 5;
+document.write(--q);// pre decrement operator print 4
