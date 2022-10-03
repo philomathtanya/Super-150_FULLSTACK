443d8bc75565788a6713c8e92ed4c3e39f066c5a
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2022-10-03 22:01:15 +0530

    Data_types

---
+++ JavaScript/Data_types.js
@@ -0,0 +1,11 @@
+ alert(NaN ** 2);// NaN=Not a number and Null^2=NaN
+
+let x = NaN;
+console.log(x, typeof x);//Print that x is NaN
+
+let isGreater = -4 > 1;//Take isGreater variable as boolean
+alert(isGreater);//False Ans print on alert
+
+let y;//Value not given here
+console.log(y, typeof y);//here type of Y is undefined
+
