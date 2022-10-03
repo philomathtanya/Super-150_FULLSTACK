be67ebd071cd3fb6d45369b80b2dae02431cb71f
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2022-10-03 22:01:37 +0530

    User_input_output

---
+++ JavaScript/User_input_output.js
@@ -0,0 +1,19 @@
+alert("HEllo this is alert");//show alert message from top
+let a = prompt("Enter the name of the college:")//take input from the alert and store in a
+alert(`College name is ${a}`)//print value of a as alert
+document.write('hello bello')//pirnt on the html page
+
+let b = '31';
+b += 1;//concatination of String is done here....number is not added
+alert(b);
+
+let c = '31';
+c = Number(c);//type casting of String type to Number by constructor
+c += 1;//Addition of  number is done here
+alert(c);
+
+let x = '30';
+let y = '51';
+alert(+x + +y);
+
+
