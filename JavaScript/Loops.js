dc9ed4512ac8c7cfe18904f65352b55e0c565dcb
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2022-10-05 23:25:17 +0530

    Loops

---
+++ JavaScript/Loops.js
@@ -0,0 +1,10 @@
+let a = 10;
+while (a > 0) { // while loop
+    document.write(a," ");
+    a--;
+}
+a = 10;
+
+for (let index = 0; index < a; index++) {  // for loop
+    document.write(index);
+}
