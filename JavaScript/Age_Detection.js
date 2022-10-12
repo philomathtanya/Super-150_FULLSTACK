28addbdb6411754d77e1285e780a5ce0b443276d
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2022-10-12 21:54:56 +0530

    Age_Detection

---
+++ JavaScript/Age_Detection.js
@@ -0,0 +1,12 @@
+let age = prompt("Enter your age : ");
+function detect() {
+    if (age < 18)
+    {
+        alert("Your age is less then 18!");
+    }
+    else
+    {
+        alert("Your age is greater then 18!");
+        }
+}
+detect();
