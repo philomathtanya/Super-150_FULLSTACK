bb70cffe31a1e26100f0b6e47c5fec63ce49bdcc
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2022-10-13 20:39:07 +0530

    Print_All_Array_element

---
+++ JavaScript/Print_All_Array_element.js
@@ -0,0 +1,12 @@
+let arr2d = [ //two d array
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+for (let i = 0; i < arr2d.length; i++)
+{
+    for (let j = 0; j < arr2d[0].length; j++){
+        console.log(arr2d[i][j]+"");
+    }
+}
