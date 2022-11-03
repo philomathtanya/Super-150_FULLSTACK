d041d9bfc271485a4635a879bfa010959976a727
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2022-11-03 23:07:40 +0530

    setTimeout_Function

---
+++ JavaScript/setTimeout_Function.js
@@ -0,0 +1,4 @@
+setTimeout(() => {
+    console.log("Hello Timeout")
+}, 3000);//3000 means that function will run after 3 second
+console.log("Bye Bye");//this will run before hello timout because hello timeout runs late
